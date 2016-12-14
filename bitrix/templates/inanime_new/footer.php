98f50fdfc3851bac254cb1509daabeafa353ee3b
filin-from-2042 <filin2042@gmail.com>
2016-12-14 11:28:55 +0300

    в футере кнопки социальных сетей включаемая область

--- bitrix/templates/inanime_new/footer.php
+++ bitrix/templates/inanime_new/footer.php
@@ -77,11 +77,11 @@
                                 Array("MODE"=>"text")
                             );?>
                         </p>
-                        <div class="social-container">
-                            <a href="http://www.facebook.com" class="social-link facebook"></a>
-                            <a href="http://www.twitter.com" class="social-link twitter"></a>
-                            <a href="http://vimeo.com" class="social-link vimeo"></a>
-                        </div>
+                        <?$APPLICATION->IncludeFile(
+                            $APPLICATION->GetTemplatePath("include_areas/socials-buttons.php"),
+                            Array(),
+                            Array("MODE"=>"html")
+                        );?>
                     </div>
 
                 </div>
