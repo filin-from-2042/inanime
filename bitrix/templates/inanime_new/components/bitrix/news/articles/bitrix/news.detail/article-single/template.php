ecb0bb1fb2190bea6950ee112137fd13b43c6958
filin-from-2042 <filin2042@gmail.com>
2016-12-15 11:41:45 +0300

    Сделана детальная страница новостей

---
+++ bitrix/templates/inanime_new/components/bitrix/news/articles/bitrix/news.detail/article-single/template.php
@@ -0,0 +1,30 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+?>
+
+<div class="container ia-news-single">
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:breadcrumb",
+        "catalog-chain",
+        Array(
+            "START_FROM" => "0",
+            "PATH" => "",
+            "SITE_ID" => SITE_ID
+        )
+    );?>
+    <div class="title-container clearfix">
+        <?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
+            <h1><?=$arResult["NAME"]?></h1>
+        <?endif;?>
+        <?if($arParams["DISPLAY_DATE"]!="N" && $arResult["DISPLAY_ACTIVE_FROM"]):?>
+            <span class="news-date"><?=$arResult["DISPLAY_ACTIVE_FROM"]?></span>
+        <?endif;?>
+    </div>
+    <div class="text-container">
+        <?if(strlen($arResult["DETAIL_TEXT"])>0):?>
+            <?echo $arResult["DETAIL_TEXT"];?>
+        <?else:?>
+            <?echo $arResult["PREVIEW_TEXT"];?>
+        <?endif?>
+    </div>
+</div>
