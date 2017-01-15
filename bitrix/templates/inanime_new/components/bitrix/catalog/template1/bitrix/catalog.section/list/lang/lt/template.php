465ae60ae6533d1d3b034d63b1cb2936a75e71e4
filin-from-2042 <filin2042@gmail.com>
2017-01-15 20:50:09 +0300

    удален неиспользуемый шаблон

--- bitrix/templates/inanime_new/components/bitrix/catalog/template1/bitrix/catalog.section/list/lang/lt/template.php
+++
@@ -1,7 +0,0 @@
-<?
-$MESS["CATALOG_BUY"] = "Pirkti";
-$MESS["CATALOG_ADD"] = "Į krepšelį";
-$MESS["CATALOG_NOT_AVAILABLE"] = "(nėra sandėlyje)";
-$MESS["CATALOG_TITLE"] = "Pavadinimas";
-$MESS["CT_BCS_ELEMENT_DELETE_CONFIRM"] = "Bus pašalinta visa informacija, susijusi su šio įrašu! Tęsti?";
-?>
