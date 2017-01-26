1132d84057f9f2956e20ffc4218952736e4f188b
filin-from-2042 <filin2042@gmail.com>
2017-01-26 11:12:55 +0300

    вопрос-ответ если нет вопросов контейнер вообще не показывается

--- personal/questions/index.php
+++ personal/questions/index.php
@@ -31,39 +31,42 @@ $APPLICATION->SetTitle("Мои вопросы и ответы");
                 </div>
 
                 <div class="col-xs-24 col-sm-16 col-md-18 col-lg-18 fields-column answers-questions-column">
-                    <div id="questions">
-                        <?
-                        CModule::IncludeModule('iblock');
-                        $userID = $USER->GetID();
-                        $arFilter = Array("IBLOCK_ID"=>25, "ACTIVE"=>"Y", 'CREATED_USER_ID'=>$userID);
-                        $res = CIBlockElement::GetList(Array('created'=>'DESC'), $arFilter,false,false,array());
-                        while($ob = $res->GetNextElement())
-                        {
-                            $props = $ob->GetProperties();
-                            ?>
-                            <div class="question-answer-section-container">
-                                <div class="question-container">
-                                    <div class="question-title">
-                                        <span class="question-text"><?=$props['question']['VALUE']['TEXT']?> ?</span>
-                                    </div>
-                                    <div class="product-link-container">
-                                        <?
-                                        $arFilterProd = Array("IBLOCK_ID"=>19, "ID"=>intval($props['question_product_id']['VALUE']));
-                                        $resProd = CIBlockElement::GetList(Array('created'=>'DESC'), $arFilterProd,false,false,array());
-                                        while($obProd = $resProd->GetNextElement())
-                                        {
-                                            $fieldsProd = $obProd->GetFields();
-                                            echo '<span class="gray-text text">Вопрос к товару</span><a href="'.$fieldsProd["DETAIL_PAGE_URL"].'" class="product-link light-blue-text-underline">'.$fieldsProd['NAME'].'</a>';
-                                        }
-                                        ?>
-                                    </div>
-                                    <div class="question-answer gray-text">
-                                        <?=($props['answer'] && $props['answer']['VALUE']) ? $props['answer']['VALUE']['TEXT'] : ''?></div>
-                                </div>
-                            </div>
+                    <?
+                    CModule::IncludeModule('iblock');
+                    $userID = $USER->GetID();
+                    $arFilter = Array("IBLOCK_ID"=>25, "ACTIVE"=>"Y", 'CREATED_USER_ID'=>$userID);
+                    $res = CIBlockElement::GetList(Array('created'=>'DESC'), $arFilter,false,false,array());
+                    if($res->SelectedRowsCount()>0)
+                    {?>
+                        <div id="questions">
                             <?
-                        }
-                        ?>
+                            while($ob = $res->GetNextElement())
+                            {
+                                $props = $ob->GetProperties();
+                                ?>
+                                <div class="question-answer-section-container">
+                                    <div class="question-container">
+                                        <div class="question-title">
+                                            <span class="question-text"><?=$props['question']['VALUE']['TEXT']?> ?</span>
+                                        </div>
+                                        <div class="product-link-container">
+                                            <?
+                                            $arFilterProd = Array("IBLOCK_ID"=>19, "ID"=>intval($props['question_product_id']['VALUE']));
+                                            $resProd = CIBlockElement::GetList(Array('created'=>'DESC'), $arFilterProd,false,false,array());
+                                            while($obProd = $resProd->GetNextElement())
+                                            {
+                                                $fieldsProd = $obProd->GetFields();
+                                                echo '<span class="gray-text text">Вопрос к товару</span><a href="'.$fieldsProd["DETAIL_PAGE_URL"].'" class="product-link light-blue-text-underline">'.$fieldsProd['NAME'].'</a>';
+                                            }
+                                            ?>
+                                        </div>
+                                        <div class="question-answer gray-text">
+                                            <?=($props['answer'] && $props['answer']['VALUE']) ? $props['answer']['VALUE']['TEXT'] : ''?></div>
+                                    </div>
+                                </div>
+                                <?
+                            }?>
+                        </div>
                         <script>
                             $(document).ready(function()
                             {
@@ -74,6 +77,7 @@ $APPLICATION->SetTitle("Мои вопросы и ответы");
                                 });
                             });
                         </script>
+                    <?}?>
                     </div>
                 </div>
             </div>
