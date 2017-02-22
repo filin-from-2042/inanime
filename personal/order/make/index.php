c23da74502f4efaa54129dd3f2bab60342baecea
konov <konov@konor.ru>
2017-02-22 15:52:09 +0300

    !FIX ОФОРМЛЕНИЕ ЗАКАЗА Правка служб доставки

--- personal/order/make/index.php
+++ personal/order/make/index.php
@@ -4,28 +4,45 @@ $APPLICATION->SetTitle("Заказы");
 $APPLICATION->AddChainItem("Оформление заказа", "");
 ?>
 <?$APPLICATION->IncludeComponent(
-    "inanime:sale.order.ajax",
-//    "bitrix:sale.order.ajax",
-    "", array(
-        "PAY_FROM_ACCOUNT" => "Y",
-        "COUNT_DELIVERY_TAX" => "N",
-        "COUNT_DISCOUNT_4_ALL_QUANTITY" => "N",
-        "ONLY_FULL_PAY_FROM_ACCOUNT" => "N",
-        "ALLOW_AUTO_REGISTER" => ($_SESSION["USER_VALUES"]["order"]["ALLOW_AUTO_REGISTER"])?$_SESSION["USER_VALUES"]["order"]["ALLOW_AUTO_REGISTER"]:"N",
-//        "ALLOW_AUTO_REGISTER" => "N",
-        "SEND_NEW_USER_NOTIFY" => "Y",
-        "DELIVERY_NO_AJAX" => "N",
-        "TEMPLATE_LOCATION" => "popup",
-        "PROP_1" => array(
-        ),
-        "PATH_TO_BASKET" => "/personal/order/make/",
-        "PATH_TO_PERSONAL" => "/personal/order/",
-        "PATH_TO_PAYMENT" => "/personal/order/payment/",
-        "PATH_TO_ORDER" => "/personal/order/make/",
-        "SET_TITLE" => "Y",
-        "DELIVERY2PAY_SYSTEM" => Array(),
-        "SHOW_ACCOUNT_NUMBER" => "Y"
-    ), false
+	"inanime:sale.order.ajax", 
+	".default", 
+	array(
+		"PAY_FROM_ACCOUNT" => "Y",
+		"COUNT_DELIVERY_TAX" => "N",
+		"COUNT_DISCOUNT_4_ALL_QUANTITY" => "N",
+		"ONLY_FULL_PAY_FROM_ACCOUNT" => "N",
+		"ALLOW_AUTO_REGISTER" => "N",
+		"SEND_NEW_USER_NOTIFY" => "Y",
+		"DELIVERY_NO_AJAX" => "N",
+		"TEMPLATE_LOCATION" => "popup",
+		"PROP_1" => array(
+		),
+		"PATH_TO_BASKET" => "/personal/order/make/",
+		"PATH_TO_PERSONAL" => "/personal/order/",
+		"PATH_TO_PAYMENT" => "/personal/order/payment/",
+		"PATH_TO_ORDER" => "/personal/order/make/",
+		"SET_TITLE" => "Y",
+		"DELIVERY2PAY_SYSTEM" => "",
+		"SHOW_ACCOUNT_NUMBER" => "Y",
+		"COMPONENT_TEMPLATE" => ".default",
+		"DELIVERY_NO_SESSION" => "N",
+		"DELIVERY_TO_PAYSYSTEM" => "d2p",
+		"USE_PREPAYMENT" => "N",
+		"PROP_5" => array(
+		),
+		"PROP_2" => array(
+		),
+		"PROP_6" => array(
+		),
+		"ALLOW_NEW_PROFILE" => "Y",
+		"SHOW_PAYMENT_SERVICES_NAMES" => "Y",
+		"SHOW_STORES_IMAGES" => "N",
+		"PATH_TO_AUTH" => "/auth/",
+		"DISABLE_BASKET_REDIRECT" => "N",
+		"PRODUCT_COLUMNS" => array(
+		)
+	),
+	false
 );
 ?>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
